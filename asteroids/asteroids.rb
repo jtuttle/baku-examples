bad7e9dba946f791d8e07267419530520fae116f
John Tuttle <jtuttle@conjur.net>
2017-10-03 13:37:14 -0400

    asteroids that break into smaller pieces when shot

--- asteroids/asteroids.rb
+++ asteroids/asteroids.rb
@@ -4,6 +4,7 @@ require 'pry'
 require 'require_all'
 
 require_relative 'game_config.rb'
+require_relative 'entity_factory.rb'
 require_all "components"
 require_all "systems"
 
@@ -13,20 +14,16 @@ class Asteroids < Gosu::Window
     self.caption = "Asteroids"
 
     @prev_ms = 0
+    
+    @world = create_world
 
-    @world = Baku::World.new
-    @world.add_system(Systems::InputSystem.new)
-    @world.add_system(Systems::LaserSystem.new)
-    @world.add_system(Systems::MovementSystem.new)
-    @world.add_system(Systems::ScreenWrapSystem.new)
-    @world.add_system(Systems::TimedDestroySystem.new)
+    EntityFactory.create_player(@world)
 
-    @world.add_system(Systems::SpriteRenderSystem.new)
-    @world.add_system(Systems::ColliderRenderSystem.new)
-
-    create_player_entity
-
-    create_asteroid_entity
+    create_starter_asteroid(100, 100)
+    create_starter_asteroid(GameConfig::SCREEN_WIDTH - 100, 100)
+    create_starter_asteroid(100, GameConfig::SCREEN_HEIGHT - 100)
+    create_starter_asteroid(GameConfig::SCREEN_WIDTH - 100,
+                            GameConfig::SCREEN_HEIGHT - 100)
   end
 
   def update
@@ -42,59 +39,27 @@ class Asteroids < Gosu::Window
 
   private
 
-  def create_player_entity
-    player = @world.create_entity
-    player.add_component(
-      PlayerInputComponent.new(
-        acceleration_speed: 0.2,
-        max_velocity: 10,
-        rotation_speed: 5,
-        friction: 0.95
-      )
-    )
-    player.add_component(
-      TransformComponent.new(
-        GameConfig::SCREEN_WIDTH / 2,
-        GameConfig::SCREEN_HEIGHT / 2,
-        10
-      )
-    )
-    player.add_component(RotationComponent.new(-90))
-    player.add_component(VelocityComponent.new(0, 0, 0))
-    player.add_component(
-      SpriteComponent.new(
-        Gosu::Image.new("assets/ship.png"),
-        scale_x: 0.5, scale_y: 0.5
-      )
-    )
-    player.add_component(
-      LaserComponent.new(
-        laser_spawn_distance: 10,
-        laser_velocity: 10,
-        laser_lifespan_ms: 500
-      )
-    )
-    player.add_component(ColliderComponent.new(25))
+  def create_world
+    world = Baku::World.new
+
+    # update systems
+    world.add_system(Systems::InputSystem.new)
+    world.add_system(Systems::LaserSystem.new)
+    world.add_system(Systems::MovementSystem.new)
+    world.add_system(Systems::ScreenWrapSystem.new)
+    world.add_system(Systems::TimedDestroySystem.new)
+    world.add_system(Systems::CollisionSystem.new)
+    world.add_system(Systems::AsteroidSplitSystem.new)
+
+    # draw systems
+    world.add_system(Systems::SpriteRenderSystem.new)
+    world.add_system(Systems::ColliderRenderSystem.new)
+
+    world
   end
 
-  def create_asteroid_entity
-    asteroid = @world.create_entity
-    asteroid.add_component(TransformComponent.new(100, 100, 10))
-    asteroid.add_component(RotationComponent.new(0))
-    asteroid.add_component(
-      VelocityComponent.new(
-        rand(-2.0...2.0),
-        rand(-2.0...2.0),
-        rand(-1.0...1.0)
-      )
-    )
-    asteroid.add_component(
-      SpriteComponent.new(
-        Gosu::Image.new("assets/asteroid.png"),
-        scale_x: 0.8, scale_y: 0.8
-      )
-    )
-    asteroid.add_component(ColliderComponent.new(50))
+  def create_starter_asteroid(x, y)
+    EntityFactory.create_asteroid(@world, 3, x, y)
   end
 end
 
